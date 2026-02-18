f421d0b4f9cf1c1bef7bf6cd6bedfd6f0c6f42f5
Melina <melina.gebhardt@student.englisches-institut.eu>
2026-02-18 13:57:16 +0100

    Versuch einer Sieg-oder-Niederlage-Prüfung, funktioniert aber noch nicht

--- Spieler.java
+++ Spieler.java
@@ -18,11 +18,15 @@ public class Spieler
         this.farbe = farbe;
         this.scanner = scanner; //lokaler Scanner wird mit allgemeinem Scanner verbunden
     }
-    public void resetUngenutzteKugeln() { //zu Beginn hat jeder Spieler 8 Kugeln
+    
+    /**
+     * zu Beginn hat jeder Spieler 8 Kugeln
+     */
+    public void resetUngenutzteKugeln() { 
         this.ungenutzteKugeln= 8;
     }
     
-        public static Spieler[] spielerZuweisung() {
+    public static Spieler[] spielerZuweisung() {
         Scanner scanner = new Scanner(System.in); //Konsole wird aufgerufen
 
         //Spieler 1
@@ -31,7 +35,6 @@ public class Spieler
         Spieler spieler1 = new Spieler(1, name1, "w", scanner); //Spieler1 wird erstellt, erhält Nummer 1 und Farbe weiß
         spieler1.resetUngenutzteKugeln(); //zu Beginn hat jeder Spieler 8 Kugel
         System.out.println(name1 + " startet mit 8 weißen Kugeln!");
-        
         
         //Spieler 2
         System.out.print("Name Spieler 2: ");
@@ -42,7 +45,10 @@ public class Spieler
         
         return new Spieler[]{spieler1, spieler2}; //beide Spieler werden erstellt
     }
-    public void kugelLegen(Spielfeld spielfeld) //der Spieler legt seine Kugeln auf das Spielfeld
+    /**
+     * der Spieler legt seine Kugeln auf das Spielfeld
+     */
+    public void kugelLegen(Spielfeld spielfeld)
     {        
         int x; //Zeilen
         int y; //Spalten
@@ -50,10 +56,10 @@ public class Spieler
         boolean gelegt = false; //zu Beginn wurden noch keine Kugeln gelegt
         
         while (!gelegt) { //wiederholt, solange noch nichts gelegt wird
-            System.out.println(name + " (" + farbe + ") , x eingeben: "); //Spieler wird nach Position x gefragt
+            System.out.println(name + " (" + farbe + ") , Zeile eingeben: "); //Spieler wird nach Position x gefragt
             x = scanner.nextInt();
             
-            System.out.println(name + " (" + farbe + ") , y eingeben: "); //Spieler wird nach Position y gefragt
+            System.out.println(name + " (" + farbe + ") , Spalte eingeben: "); //Spieler wird nach Position y gefragt
             y = scanner.nextInt();
             
             gelegt = spielfeld.platzPruefen(x, y, this); //Es wird geprüft, ob man eine Kugeln auf diese Position legen darf
--- Spielfeld.java
+++ Spielfeld.java
@@ -16,9 +16,11 @@ public class Spielfeld
     boolean spielFertig = false; //das Spiel ist von Beginn an nicht fertig
     private int xFeldLaenge = 4; //Länge einer Zeile auf dem Spielfeld
     private int yFeldLaenge = 4; //Länger einer Spalte auf dem Spielfeld
+    private int kugelstatus = 0; //prüft, wie viele Kugeln der gleichen Sorte nebeneinander sind
     boolean horizontal = false;  //Variable, die speichert ob vier gleiche Kugeln horizontal nebeneinander sind
     boolean vertikal = false;    //Variable, die speichert ob vier gleiche Kugeln vertikal nebeneinander sind
     boolean diagonal = false;    //Variable, die speichert ob vier gleiche Kugeln diagonal nebeneinander sind
+    boolean kugelnNebeneinander = false;
     /**
      * Konstruktor für Objekte der Klasse Spielfeld
      */
@@ -34,11 +36,14 @@ public class Spielfeld
         
         aktuellerSpieler = spieler1;
         anzeige();
-        pruefeSpielzustand();
+        spielAblauf();
        
     }
     
-    public void anzeige () { //Spielfeld wird angezeigt
+    /**
+     * Spielfeld wird angezeigt
+     */
+    public void anzeige () { 
         System.out.println("  0 1 2 3"); //Spaltennummerierung
         for (int x = 0; x < 4 ; x++) { // Jede neue Zeile wird von 0-3 mit Leerzeichen nummeriert
             String spalte = "" + x; 
@@ -70,7 +75,10 @@ public class Spielfeld
         return true;
     }
     
-    public void spielAblauf() { //hier werden die Spieler abwechselnd nach den Kugelkoordinaten gefragt
+    /**
+     * hier werden die Spieler abwechselnd nach den Kugelkoordinaten gefragt
+     */
+    public void spielAblauf() { 
             while (!spielFertig) { //solange das Spiel nicht fertig ist
             // Spieler nach Koordinaten fragen
             aktuellerSpieler.kugelLegen(this); // Die Methode fragt x und y ab und setzt die Kugel
@@ -98,16 +106,85 @@ public class Spielfeld
         }
         return true; //wenn kein Feld leer war, ist das Spielfeld voll
     }
-
-   
-    private void pruefeSpielzustand() { //hier wird geprüft,ob das Spiel gewonnen, verloren oder unentschieden ist
-        if (spielfeldVoll()) { //wenn das Spielfeld voll ist und es vorher keinen Sieg gab, ist das Spiel unentschieden
-            spielFertig = true; //das Spiel ist fertig und damit wird spielAblauf beendet
-            System.out.println("Unentschieden!");
-        } 
-        if (horizontal || vertikal || diagonal) {
-            System.out.println(spieler1 + "hat gewonnen!");
-            System.out.println(spieler2 + "hat verloren!");
-        } 
+    
+    public boolean kugelnNebeneinander(int x,int y) {
+        //Woran kann der Computer erkennen,dass vier Kugeln nebeneinander sind?:
+        //derselbe spieler, eine spalte/zeile/zwei diagonalen
+        //eine zeile: (0 |0 ) (0 |1 ) (0 |2 ) (0 |3 )
+        //Zeilenformel: alle y- werte unterschiedlich, alle x-werte gleich (x|y+1 ) 
+        //eine spalte: (0 |0 ) (1 |0 ) (2 |0 ) (3 |0 )
+        //Spaltenformel: alle x- werte unterschiedlich, alle y-werte gleich (x+1|y) 
+        //eine Diagonale: 1. (0 |0 ) (1 |1 ) (2 |2 ) (3 |3 ) von links oben nach rechts unten
+        //zweite Diagonale: (3 | 0)  (2 |1 ) (1 |2 ) (0 |3 ) von rechts oben nach links unten
+        //Diagonalenformel: ( n | n) bzw. x==y oder ( n-1 | n+1);  
+            
+            //horizontal/Zeile
+        if (y <= xFeldLaenge - 4) { //wenn genug Platz nach rechts ist (4 Felder insgesamt)
+            Kugeln k1 = spielbrett[x][y];
+            Kugeln k2 = spielbrett[x][y+1];
+            Kugeln k3 = spielbrett[x][y+2];
+            Kugeln k4 = spielbrett[x][y+3];
+             if (k1 != null && k1.equalsFarbe(k2) && k1.equalsFarbe(k3) && k1.equalsFarbe(k4)) {
+                System.out.println("Alle vier Kugeln von " + k1.color + " sind horizontal nebeneinander!");
+                System.out.println(spieler1 + "hat gewonnen!");
+                spielFertig = true;
+            }
+        }
+        if (spielbrett[x][y] == spielbrett[x+1][y]){ //vertikal/Spalte
+                System.out.println("Alle vier Kugeln von" + spieler1 + "sind vertikal nebeneinander!");
+                System.out.println(spieler1 + "hat gewonnen!");
+                spielFertig = true;
+                return true;
+            }
+        if (spielbrett[x][y] == spielbrett[x][y+1]){ //diagonal
+                System.out.println("Alle vier Kugeln von" + spieler1 + "sind diagonal nebeneinander!");
+                System.out.println(spieler1 + "hat gewonnen!");
+                spielFertig = true;
+                return true;
+        }
+          
+        // Vertikal/Spalte
+        if (x <= xFeldLaenge - 4) {
+            Kugeln k1 = spielbrett[x][y];
+            Kugeln k2 = spielbrett[x+1][y];
+            Kugeln k3 = spielbrett[x+2][y];
+            Kugeln k4 = spielbrett[x+3][y];
+            if (k1 != null && k1.equalsFarbe(k2) && k1.equalsFarbe(k3) && k1.equalsFarbe(k4)) {
+                System.out.println("Vertikal gewonnen!");
+                spielFertig = true;
+                return true;
+            }
+        }
+        
+        // Diagonal rechts unten prüfen
+        if (x <= xFeldLaenge - 4 && y <= yFeldLaenge - 4) {
+            Kugeln k1 = spielbrett[x][y];
+            Kugeln k2 = spielbrett[x+1][y+1];
+            Kugeln k3 = spielbrett[x+2][y+2];
+            Kugeln k4 = spielbrett[x+3][y+3];
+            if (k1 != null && k1.equalsFarbe(k2) && k1.equalsFarbe(k3) && k1.equalsFarbe(k4)) {
+                System.out.println("Diagonal gewonnen!");
+                spielFertig = true;
+                return true;
+            }
+        }
+        return false;         
     }
+    
+        /**
+         * hier wird geprüft,ob das Spiel gewonnen, verloren oder unentschieden ist
+         */
+    private void pruefeSpielzustand() { 
+            if (spielfeldVoll()) { //wenn das Spielfeld voll ist und es vorher keinen Sieg gab, ist das Spiel unentschieden
+                spielFertig = true; //das Spiel ist fertig und damit wird spielAblauf beendet
+                System.out.println("Unentschieden!");
+            } 
+            if (kugelnNebeneinander) {
+                System.out.println("Sieg");
+                //System.out.println(spieler1 + "hat gewonnen!");
+                //System.out.println(spieler2 + "hat verloren!");
+            } 
+    }
+  
 }
+
