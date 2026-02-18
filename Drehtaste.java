8d8fd6f7966b63ba7388beced82293b731a0ca71
Georg <georg.bernhardt@student.englisches-institut.eu>
2026-02-18 15:37:01 +0100

    orbito Knopf hinzugefügt

---
+++ Drehtaste.java
@@ -0,0 +1,32 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Drehtaste.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Drehtaste
+{
+    /**
+     * Konstruktor für Objekte der Klasse Drehtaste
+     */
+    public Drehtaste()
+    {
+        drehen();
+    }
+    
+    public void drehen() {
+        /*
+        public static void rotateRight(int[] arr) {
+        if (arr == null || arr.length <= 1) return;
+            int last = arr[arr.length - 1]; // Letztes Element speichern
+        for (int i = arr.length - 1; i > 0; i--) {
+            arr[i] = arr[i - 1]; // Nach rechts verschieben
+        }
+        arr[0] = last; // Gespeichertes Element an den Anfang
+        */ 
+        // Herr Flörchi meinte, dieses Stück code sollte im großen Teil richtig sein, nur noch anpassen muss man
+    }
+
+    }
+
