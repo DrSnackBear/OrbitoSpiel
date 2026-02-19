61d43657e20b7f7908d8a6080c2f0cd2645aebf4
Georg <georg.bernhardt@student.englisches-institut.eu>
2026-02-19 17:30:51 +0100

    kugeln codierpoblem gelöst

--- Kugeln.java
+++ Kugeln.java
@@ -18,7 +18,10 @@ public class Kugeln
         //this.kugelNummer = kugelNummer;
     }
         public boolean equalsFarbe(Kugeln andere) {
-        if (andere == null) return false;       // andere Kugel existiert nicht
+        if (andere == null) {
+            return false;     
+        }  // andere Kugel existiert nicht
+        
         return this.color.equals(andere.color); // Vergleich der Farben
     }
 }
